d4e4f30660b29c56ae5fde3912a51b51e5c54a30
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2020-08-14 21:57:23 +0000

    Automatically backed up by Learn

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,23 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
+  const renderMovies = () => (
+    movies.map(({title, time, genres}) => (
+      <Movie
+        title={title}
+        time={time}
+        genres={genres}
+      />
+    ))
+  )
+
   return (
     <div>
-        {/*{code here}*/}
+      <React.Fragment>
+        <h1>Movies Page</h1>
+        {renderMovies()}
+  </React.Fragment>
     </div>
   );
 };
